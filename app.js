69a82efef1113d445c246979cfd8684eaeeb27fb
kaichen <3069394597@qq.com>
2024-04-18 14:38:22 +0800

    重新部署

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const bodyParser = require('body-parser');
 const express = require('express');
-const serverless = require('serverless-http');
 const app = express();
+const port = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-let ArticleContent = [
-   {
+let data = [
+ {
         id:1,
         essaysTitle: "这是一篇测试内容",
         detail: "长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试长文本测试",
@@ -105,34 +101,10 @@ let ArticleContent = [
         }
 ];
 
-app.get('/api/getArticles', (req, res) => {
-    let id = req.query.id;
-
-    if (typeof id === 'undefined' || id === null) {
-    // 当 id 没有传值过来时，返回 ArticleContent 所有数据
-    res.send({
-        message: 'get 接口请求成功',
-        code: 200,
-        data: ArticleContent
-    });
-    } else {
-    let arr = ArticleContent.filter(item => item.id == id);
-    res.send({
-        message: 'get 接口请求成功',
-        code: 200,
-        data: arr
-    });
-}
+app.get('/api/data', (req, res) => {
+  res.json(data);
 });
 
-module.exports = app;
-
-if (process.env.VERCEL) {
-    module.exports.handler = serverless(app);
-}
-
-//    const port = process.env.PORT || 3000;
-
-//    app.listen(port, () => {
-//      console.log(`Server is running on port ${port}`);
-//    });
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
